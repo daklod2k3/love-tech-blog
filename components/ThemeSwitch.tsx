d3b4e989ba13137eff560e89b0f3aeca52b6a17b
Dak Lod <trandacloc123@gmail.com>
2024-07-14 19:54:17 +0700

    change ThemeSwitcher to toggle instead of menu"

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { Fragment, useEffect, useState } from 'react'
-import { useTheme } from 'next-themes'
 import { Menu, RadioGroup, Transition } from '@headlessui/react'
+import { useTheme } from 'next-themes'
+import { Fragment, MutableRefObject, useEffect, useRef, useState } from 'react'
+import { flushSync } from 'react-dom'
 
 const Sun = () => (
   <svg
@@ -49,83 +50,36 @@ const Blank = () => <svg className="h-6 w-6" />
 const ThemeSwitch = () => {
   const [mounted, setMounted] = useState(false)
   const { theme, setTheme, resolvedTheme } = useTheme()
+  const switchRef = useRef<HTMLButtonElement>(null)
+
+  const themeSwitcher = () => {
+    if (mounted && switchRef.current) {
+      document.startViewTransition(() =>
+        // flushSync(() => (resolvedTheme === 'dark' ? setTheme('light') : setTheme('dark')))
+        resolvedTheme === 'dark' ? setTheme('light') : setTheme('dark')
+      ).ready
+
+      // const { top, left } = switchRef.current.getBoundingClientRect()
+
+      // document.documentElement.animate(
+      //   { clipPath: [`circle(Opx at ${left}px ${top}px)`, `circle(2000px at ${left}px ${top}px)`] },
+      //   { duration: 500, easing: 'ease-in-out', pseudoElement: '::view-transition-new(root)' }
+      // )
+    }
+  }
 
   // When mounted on client, now we can show the UI
   useEffect(() => setMounted(true), [])
 
   return (
     <div className="mr-5 flex items-center">
-      <Menu as="div" className="relative inline-block text-left">
-        <div className="flex items-center justify-center hover:text-primary-500 dark:hover:text-primary-400">
-          <Menu.Button>
-            {mounted ? resolvedTheme === 'dark' ? <Moon /> : <Sun /> : <Blank />}
-          </Menu.Button>
-        </div>
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="transform opacity-0 scale-95"
-          enterTo="transform opacity-100 scale-100"
-          leave="transition ease-in duration-75"
-          leaveFrom="transform opacity-100 scale-100"
-          leaveTo="transform opacity-0 scale-95"
-        >
-          <Menu.Items className="absolute right-0 z-50 mt-2 w-32 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-gray-800">
-            <RadioGroup value={theme} onChange={setTheme}>
-              <div className="p-1">
-                <RadioGroup.Option value="light">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <button
-                        className={`${
-                          active ? 'bg-primary-600 text-white' : ''
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        <div className="mr-2">
-                          <Sun />
-                        </div>
-                        Light
-                      </button>
-                    )}
-                  </Menu.Item>
-                </RadioGroup.Option>
-                <RadioGroup.Option value="dark">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <button
-                        className={`${
-                          active ? 'bg-primary-600 text-white' : ''
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        <div className="mr-2">
-                          <Moon />
-                        </div>
-                        Dark
-                      </button>
-                    )}
-                  </Menu.Item>
-                </RadioGroup.Option>
-                <RadioGroup.Option value="system">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <button
-                        className={`${
-                          active ? 'bg-primary-600 text-white' : ''
-                        } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                      >
-                        <div className="mr-2">
-                          <Monitor />
-                        </div>
-                        System
-                      </button>
-                    )}
-                  </Menu.Item>
-                </RadioGroup.Option>
-              </div>
-            </RadioGroup>
-          </Menu.Items>
-        </Transition>
-      </Menu>
+      <button
+        ref={switchRef}
+        className="flex items-center justify-center hover:text-primary-500 dark:hover:text-primary-400"
+        onClick={themeSwitcher}
+      >
+        {mounted ? resolvedTheme === 'dark' ? <Moon /> : <Sun /> : <Blank />}
+      </button>
     </div>
   )
 }
